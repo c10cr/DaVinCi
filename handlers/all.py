659945ed0919eb3f208918b65418638b25c79b67
ItsDAVINCI <75629708+c10cr@users.noreply.github.com>
2022-04-22 20:34:07 +0300

    Add files via upload

--- handlers/all.py
+++ handlers/all.py
@@ -56,7 +56,7 @@ def allGP(client, message,redis):
         Bot("sendMessage",{"chat_id":chatID,"text":r.userNocc,"reply_to_message_id":message.message_id,"parse_mode":"html"})
 
     if re.search(c.sors,text):
-      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/"+BOTATE)],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/RSaiedBot")],[InlineKeyboardButton("قناة البوتات 🤖", url="t.me/BOTATE")]])
+      kb = InlineKeyboardMarkup([[InlineKeyboardButton("قناه السورس 📢", url="t.me/"+vvvnn)],[InlineKeyboardButton("تواصل السورس 💬", url="t.me/c10cr")],[InlineKeyboardButton("قناة البوتات 🤖", url="t.me/vvvnn")]])
       Botuser = client.get_me().username
       Bot("sendMessage",{"chat_id":chatID,"text":r.sors.format("@"+Botuser),"disable_web_page_preview":True,"reply_to_message_id":message.message_id,"parse_mode":"markdown","reply_markup":kb})
     
